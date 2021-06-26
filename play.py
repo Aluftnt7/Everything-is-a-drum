04602e1a0099fdf7fcc3640f82275ea13a96eb85
yanirqt <yanirqt@gmail.com>
2021-06-26 13:23:56 +0300

    use env var for play path

--- play.py
+++ play.py
@@ -2,23 +2,22 @@ import pygame.mixer
 from time import sleep
 from pynput import keyboard
 import time
-import os.path
-from os import path
+import os
 
 set_idx = 0
 sound_arr_a = ["kick.ogg","clap.ogg","hat.ogg", "openhat.ogg", "snr.ogg", "left.ogg", "right.ogg", "up.ogg","down.ogg"]
-sets = ["elctroset", "acoustic", "vinyl", "farts", "punch", "family_guy", "tank_drum"]
+sets = ["elctroset", "acoustic", "vinyl", "farts", "punch", "tank_drum"]
 keys_dict = {"w" : 0, "a" : 1, "s" : 2, "d" : 3, "space" : 4, "right" : 5, "up" : 6, "left" : 7, "down" : 8}
-sounds ={"elctroset": [], "acoustic":[], "vinyl":[] , "farts":[], "punch":[], "family_guy":[], "tank_drum":[]}
+sounds ={"elctroset": [], "acoustic":[], "vinyl":[] , "farts":[], "punch":[], "tank_drum":[]}
 
 def load_sounds():
+    base_path = os.getenv("DRUM_HOME","/home/pi/Desktop/burn")
     for set in sets:
         for sound in sound_arr_a:
-            path_exists = get_path_exists("/home/pi/Desktop/burn/"+ set + "/" + sound) 
+            path_exists = os.path.exists(base_path + "/" + set + "/" + sound)
             if(path_exists):
-                sounds.get(set).append(pygame.mixer.Sound("/home/pi/Desktop/burn/"+ set + "/" + sound))
+                sounds.get(set).append(pygame.mixer.Sound(base_path + "/" + set + "/" + sound))
           
-
 def play_key(key):
     
     if(key in keys_dict and keys_dict[key] < len(sounds[sets[set_idx]])):
@@ -41,11 +40,6 @@ def on_press(key):
         on_changing_set()
     play_key(key)
 
-def get_path_exists(path):
-    path_exists =  os.path.exists(path)
-    return path_exists
-
-
 def on_release(key):
     if key == keyboard.Key.esc:
         print("Done!")
@@ -67,5 +61,4 @@ def main():
   
 if __name__== "__main__":
       main()
-
 
