90821cd77a6a0d2f39379a069c6d8ffcfb959589
yanirqt <yanirqt@gmail.com>
2021-06-26 19:11:17 +0300

    add family guy in play

--- play.py
+++ play.py
@@ -6,9 +6,9 @@ import os
 
 set_idx = 0
 sound_arr_a = ["kick.ogg","clap.ogg","hat.ogg", "openhat.ogg", "snr.ogg", "left.ogg", "right.ogg", "up.ogg","down.ogg"]
-sets = ["elctroset", "acoustic", "vinyl", "farts", "punch", "tank_drum"]
+sets = ["family_guy","elctroset", "acoustic", "vinyl", "farts", "punch", "tank_drum"]
 keys_dict = {"w" : 0, "a" : 1, "s" : 2, "d" : 3, "space" : 4, "right" : 5, "up" : 6, "left" : 7, "down" : 8}
-sounds ={"elctroset": [], "acoustic":[], "vinyl":[] , "farts":[], "punch":[], "tank_drum":[]}
+sounds ={"family_guy": [],"elctroset": [], "acoustic":[], "vinyl":[] , "farts":[], "punch":[], "tank_drum":[]}
 
 def load_sounds():
     base_path = os.getenv("DRUM_HOME","/home/pi/Desktop/burn")
@@ -19,7 +19,6 @@ def load_sounds():
                 sounds.get(set).append(pygame.mixer.Sound(base_path + "/" + set + "/" + sound))
           
 def play_key(key):
-    
     if(key in keys_dict and keys_dict[key] < len(sounds[sets[set_idx]])):
         #pygame.mixer.Channel(keys_dict[key]).play(pygame.mixer.Sound(sounds[sets[set_idx]][keys_dict[key]]))
         pygame.mixer.find_channel(True).play(pygame.mixer.Sound(sounds[sets[set_idx]][keys_dict[key]]))
