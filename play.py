93cc01f97785ec477484cafe3f2ff40ddc856611
Tamir Nachshon <tamirn7@gmail.com>
2021-06-26 12:52:04 +0300

    added more sounds

--- play.py
+++ play.py
@@ -1,21 +1,30 @@
 import pygame.mixer
 from time import sleep
 from pynput import keyboard
+import time
+import os.path
+from os import path
 
 set_idx = 0
-sound_arr_a = ["kick.ogg","clap.ogg","hat.ogg", "openhat.ogg", "snr.ogg"]
-sets = ["elctroset", "acoustic", "vinyl", "farts"]
-keys_dict = {"w" : 0, "a" : 1, "s" : 2, "d" : 3, "space" : 4}
-sounds ={"elctroset": [], "acoustic":[], "vinyl":[] , "farts":[]}
+sound_arr_a = ["kick.ogg","clap.ogg","hat.ogg", "openhat.ogg", "snr.ogg", "left.ogg", "right.ogg", "up.ogg","down.ogg"]
+sets = ["elctroset", "acoustic", "vinyl", "farts", "punch", "family_guy", "tank_drum"]
+keys_dict = {"w" : 0, "a" : 1, "s" : 2, "d" : 3, "space" : 4, "right" : 5, "up" : 6, "left" : 7, "down" : 8}
+sounds ={"elctroset": [], "acoustic":[], "vinyl":[] , "farts":[], "punch":[], "family_guy":[], "tank_drum":[]}
 
 def load_sounds():
     for set in sets:
         for sound in sound_arr_a:
-            sounds.get(set).append(pygame.mixer.Sound("./"+ set + "/" + sound))
+            path_exists = get_path_exists("/home/pi/Desktop/burn/"+ set + "/" + sound) 
+            if(path_exists):
+                sounds.get(set).append(pygame.mixer.Sound("/home/pi/Desktop/burn/"+ set + "/" + sound))
+          
 
 def play_key(key):
-    if(key in keys_dict):
-        pygame.mixer.Sound.play(sounds[sets[set_idx]][keys_dict[key]])
+    
+    if(key in keys_dict and keys_dict[key] < len(sounds[sets[set_idx]])):
+        #pygame.mixer.Channel(keys_dict[key]).play(pygame.mixer.Sound(sounds[sets[set_idx]][keys_dict[key]]))
+        pygame.mixer.find_channel(True).play(pygame.mixer.Sound(sounds[sets[set_idx]][keys_dict[key]]))
+    
         
 def on_changing_set():
         global set_idx
@@ -32,6 +41,10 @@ def on_press(key):
         on_changing_set()
     play_key(key)
 
+def get_path_exists(path):
+    path_exists =  os.path.exists(path)
+    return path_exists
+
 
 def on_release(key):
     if key == keyboard.Key.esc:
@@ -45,8 +58,10 @@ def wait_for_user_input():
     listener.join() # wait for listener stop
 
 def main():
+    pygame.mixer.pre_init(48000, -16, len(keys_dict), 1024)
+    pygame.mixer.quit()
     pygame.init()
-    pygame.mixer.init(48000, -16, 1, 1024)
+    pygame.mixer.init(48000, -16, len(keys_dict), 1024)
     load_sounds()
     wait_for_user_input()
   
